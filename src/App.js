66c9a36424788b91290452b91271fe0694ddf0ef
GouriNanda <gourinanda222@gmail.com>
2024-08-15 19:38:55 +0530

    discountcalculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Discount from './Discount';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <img src='https://media2.giphy.com/media/Pm9UQQBQbtLe1scjOQ/source.gif'></img>
+
+      </div>
+    <Discount/>
     </div>
   );
 }
